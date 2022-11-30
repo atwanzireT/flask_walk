e097b609642d6c4535d8da48b45c9c6dc47e5073
atwanzireT <tatwanzire@gmail.com>
2022-11-30 10:25:19 +0300

    flask

--- views.py
+++ views.py
@@ -1,25 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
-from models.models import *
-from views import views
-
-app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
-# db = SQLAlchemy(app)
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# create model
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(50), nullable = False)
-    body = db.Column(db.String(500))
-    created = db.Column(db.DateTime, default = datetime.utcnow())
-
-
-    def __repr__(self):
-        return '<Title %r>' % self.title
-        
 @app.route('/')
 def index():
     title = "Fedora's Blog"
@@ -44,7 +26,3 @@ def form():
         return render_template("fail.html", error_statement = error_statement)
 
     return render_template('form.html', name = name)
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
